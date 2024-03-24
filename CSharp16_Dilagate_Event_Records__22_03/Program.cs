2802fb447ac5de6204e7cb91a990f3886ce1ea7e
MaksimMyson <maksimmyson33@gmail.com>
2024-03-24 14:40:54 +0200

    ex7 was done

--- CSharp16_Dilagate_Event_Records__22_03/Program.cs
+++ CSharp16_Dilagate_Event_Records__22_03/Program.cs
@@ -1,10 +1,24 @@
 ﻿namespace CSharp16_Dilagate_Event_Records__22_03
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            
+            Func<int[], int[]> findOddNumbers = arr => arr.Where(num => num % 2 != 0).ToArray();
+
+            
+            int[] numbers = { 3, 7, 2, 9, 1, 5 };
+
+            int[] oddNumbers = findOddNumbers(numbers);
+
+            Console.WriteLine("Непарні числа у масиві:");
+
+            foreach (int num in oddNumbers)
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
