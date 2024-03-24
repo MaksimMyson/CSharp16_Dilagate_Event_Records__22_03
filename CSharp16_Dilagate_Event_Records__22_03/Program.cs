5ab04cdf814cb267ed1c7461bc61cf417497b9e3
MaksimMyson <maksimmyson33@gmail.com>
2024-03-24 14:39:11 +0200

    ex6 was done

--- CSharp16_Dilagate_Event_Records__22_03/Program.cs
+++ CSharp16_Dilagate_Event_Records__22_03/Program.cs
@@ -1,10 +1,17 @@
 ﻿namespace CSharp16_Dilagate_Event_Records__22_03
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            
+            Func<int[], int> findMin = arr => arr.Min();
+
+            
+            int[] numbers = { 3, 7, 2, 9, 1, 5 };
+
+            Console.WriteLine($"Мінімум у масиві: {findMin(numbers)}");
         }
     }
 }
