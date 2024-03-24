3a5a6c3c558d06c63c210ade91ddafe9c00f8be7
MaksimMyson <maksimmyson33@gmail.com>
2024-03-24 14:29:34 +0200

    Add project files.

---
+++ CSharp16_Dilagate_Event_Records__22_03/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp16_Dilagate_Event_Records__22_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
