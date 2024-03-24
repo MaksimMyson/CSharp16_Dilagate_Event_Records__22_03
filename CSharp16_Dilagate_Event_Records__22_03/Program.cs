b8d2dc5d2c95c83ce0f8b1d62e4e256d117200c8
MaksimMyson <maksimmyson33@gmail.com>
2024-03-24 14:31:27 +0200

    ex1 was done

--- CSharp16_Dilagate_Event_Records__22_03/Program.cs
+++ CSharp16_Dilagate_Event_Records__22_03/Program.cs
@@ -1,10 +1,21 @@
 ﻿namespace CSharp16_Dilagate_Event_Records__22_03
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            
+            Func<int, bool> isEven = delegate (int number)
+            {
+                return number % 2 == 0;
+            };
+
+            
+            int num1 = 10;
+            int num2 = 7;
+
+            Console.WriteLine($"{num1} is {(isEven(num1) ? "even" : "odd")}");
+            Console.WriteLine($"{num2} is {(isEven(num2) ? "even" : "odd")}");
         }
     }
 }
