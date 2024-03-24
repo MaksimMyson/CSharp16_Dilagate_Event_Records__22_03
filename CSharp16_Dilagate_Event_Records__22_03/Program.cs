204b5c0fc8bfd8d178b40b01d0a86b5921fbb9d0
MaksimMyson <maksimmyson33@gmail.com>
2024-03-24 14:32:44 +0200

    ex2 was done

--- CSharp16_Dilagate_Event_Records__22_03/Program.cs
+++ CSharp16_Dilagate_Event_Records__22_03/Program.cs
@@ -1,10 +1,19 @@
 ﻿namespace CSharp16_Dilagate_Event_Records__22_03
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            
+            Func<int, int> square = delegate (int number)
+            {
+                return number * number;
+            };
+
+            
+            int num = 5;
+
+            Console.WriteLine($"Square of {num}: {square(num)}");
         }
     }
 }
