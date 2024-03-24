b0452ba726202c0752ea2cc1d5b85bebe21cb6b9
MaksimMyson <maksimmyson33@gmail.com>
2024-03-24 14:35:23 +0200

    ex4 was done

--- CSharp16_Dilagate_Event_Records__22_03/Program.cs
+++ CSharp16_Dilagate_Event_Records__22_03/Program.cs
@@ -1,10 +1,18 @@
 ﻿namespace CSharp16_Dilagate_Event_Records__22_03
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            
+            Func<DateTime, bool> isProgrammersDay = date => date.DayOfYear == 256;
+
+           
+            DateTime testDate1 = new DateTime(2024, 9, 12); 
+            DateTime testDate2 = new DateTime(2024, 9, 13); 
+
+            Console.WriteLine($"Is {testDate1:yyyy-MM-dd} Programmers' Day? {isProgrammersDay(testDate1)}");
+            Console.WriteLine($"Is {testDate2:yyyy-MM-dd} Programmers' Day? {isProgrammersDay(testDate2)}");
         }
     }
 }
