ed5e0bf17b83b6cc5c37a82ec16c8c1bcf0b19df
MaksimMyson <maksimmyson33@gmail.com>
2024-03-29 15:12:08 +0200

    ex6 was done

--- CSharp16_Dilagate_Event_Records__22_03/Program.cs
+++ CSharp16_Dilagate_Event_Records__22_03/Program.cs
@@ -1,10 +1,32 @@
 ﻿namespace CSharp16_Dilagate_Event_Records__22_03
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+                
+            
+            Console.Write("Введіть слово для перевірки: ");
+            string targetWord = Console.ReadLine();
+
+            
+            Func<string, string, bool> checkForWord = (text, word) => text.ToLower().Contains(word.ToLower());
+
+           
+            Console.WriteLine("Введіть текст для перевірки:");
+            string testText = Console.ReadLine(); 
+            bool containsWord = checkForWord(testText, targetWord); 
+
+          
+            if (containsWord)
+            {
+                Console.WriteLine($"Текст містить слово '{targetWord}'.");
+            }
+            else
+            {
+                Console.WriteLine($"Текст не містить слово '{targetWord}'.");
+            }
         }
     }
 }
