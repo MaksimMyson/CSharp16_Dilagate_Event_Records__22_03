80bc5080e10fcdcc67e13fb1cd6f3e4839556cbb
MaksimMyson <maksimmyson33@gmail.com>
2024-03-29 14:46:12 +0200

    ex 4 was done

--- CSharp16_Dilagate_Event_Records__22_03/Program.cs
+++ CSharp16_Dilagate_Event_Records__22_03/Program.cs
@@ -1,10 +1,35 @@
 ﻿namespace CSharp16_Dilagate_Event_Records__22_03
 {
-    internal class Program
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+           Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Введіть числа, розділені пробілами:");
+            string input = Console.ReadLine();
+            string[] numbersAsString = input.Split(' ');
+            int[] numbers = Array.ConvertAll(numbersAsString, int.Parse);
+
+           
+            Console.WriteLine("Масив:");
+            foreach (var num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+
+            
+            Func<int[], int> countPositiveNumbers = arr => Array.FindAll(arr, x => x > 0).Length;
+           
+
+            
+            int count = countPositiveNumbers(numbers);
+            Console.WriteLine($"Кількість позитивних чисел у масиві: {count}");
+
+        
         }
     }
 }
+
