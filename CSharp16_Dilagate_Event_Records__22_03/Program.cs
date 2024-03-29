a25001970b3bc7222f6ce1d61d4535dd9ee8d8db
MaksimMyson <maksimmyson33@gmail.com>
2024-03-29 15:02:31 +0200

    ex5 was done

--- CSharp16_Dilagate_Event_Records__22_03/Program.cs
+++ CSharp16_Dilagate_Event_Records__22_03/Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace CSharp16_Dilagate_Event_Records__22_03
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            
+            Func<int[], int[]> uniqueNegativeNumbers = arr => arr.Where(x => x < 0).Distinct().ToArray();
+
+            
+            int[] testArray = { -2, 5, -2, 3, -5, 7, -5 };
+            int[] uniqueNegatives = uniqueNegativeNumbers(testArray); 
+
+            
+            Console.WriteLine("Унікальні негативні числа у масиві:");
+            foreach (var num in uniqueNegatives)
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
