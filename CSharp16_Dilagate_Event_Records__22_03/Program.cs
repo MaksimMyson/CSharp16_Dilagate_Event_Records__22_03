c95d30d1e536215d22bcf797ac979bd83c61c7a0
MaksimMyson <maksimmyson33@gmail.com>
2024-03-29 14:14:48 +0200

    ex3 was done

--- CSharp16_Dilagate_Event_Records__22_03/Program.cs
+++ CSharp16_Dilagate_Event_Records__22_03/Program.cs
@@ -1,10 +1,17 @@
 ﻿namespace CSharp16_Dilagate_Event_Records__22_03
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            
+            Func<int[], int> countMultiplesOfSeven = arr => Array.FindAll(arr, x => x % 7 == 0).Length;
+
+           
+            int[] testArray = { 7, 14, 21, 28, 35, 42, 8, 10, 49 }; 
+            int count = countMultiplesOfSeven(testArray); 
+            Console.WriteLine($"Кількість чисел у масиві, кратних семи: {count}");
         }
     }
 }
